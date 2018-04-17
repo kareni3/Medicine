0923fbec493cb3a3380764252f65a807a4acc842
Elisey <selisej@yandex.ru>
2018-04-17 03:08:33 +0300

    Medicament и Patient теперь хранятся в базе данных MS SQL Server. Реализовано подключение к БД, а также Update и Insert для них.

---
+++ OpenEHR/SqlConnection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenEHR
+{
+	public class SqlConnection
+	{
+		System.Data.SqlClient.SqlConnection Connection;
+		public string Server { get; set; }
+		public string Port { get; set; }
+		public string User { get; set; }
+		public string Password { private get; set; }
+
+		private bool connected;
+
+		public SqlConnection()
+		{
+			connected = false;
+		}
+
+		public SqlConnection(string server, string port, string user, string password) : this()
+		{
+			Server = server;
+			Port = port;
+			User = user;
+			Password = password;
+		}
+
+		public void Connect()
+		{
+			try
+			{
+				Connection = new System.Data.SqlClient.SqlConnection(String.Format(ConfigurationManager.ConnectionStrings["openehr"].ConnectionString, Server, Port, User, Password));
+				Connection.Open();
+				connected = true;
+			}
+			catch
+			{
+				connected = false;
+				throw;
+			}
+		}
+
+		internal System.Data.SqlClient.SqlCommand GetCommand()
+		{
+			if (!connected)
+				throw new Exception("Не передан экземпляр SqlConnection");
+			return new System.Data.SqlClient.SqlCommand() { Connection = Connection };
+		}
+	}
+}
